d779e4655c160c14a61fd22ea7f5454fd0e9699e
Ryan Whetstone <whetstone.ryan@gmail.com>
2025-12-13 21:17:14 -0500

    Add missing signIn import in signup page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
 
 export default function SignUpPage() {
